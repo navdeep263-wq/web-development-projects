944f24332d6caaf30938bda7a31f0ed64316e519
navdeep263-wq <navdeepkaur83882@gmail.com>
2026-06-03 23:21:22 +0530

    practice javaScript loops

---
+++ learn-javaScript/loops.js
@@ -0,0 +1,56 @@
+// today practice and learn  loops in javaScript
+
+//for loop
+//print 1 to 5
+// for(let i = 1; i <=5; i++){
+//     console.log("hello");
+// }
+
+//calculate sum of 1 to n numbers
+// let sum = 0;
+//  for(let i = 1; i <= 5; i++){
+//     sum = sum + i;
+    
+//  }
+//  console.log(sum);
+
+// using while loop print 1 to 5 numbers
+// let i = 1;
+// while(i <= 5){
+//      console.log( "i = " + i);
+//      i++;
+// }
+
+//do while
+// let i = 20;
+// do{
+//     console.log(i);
+//     i++;
+// } while(i <= 10);
+
+//print 1 to 5 numbers using do while
+// let i = 1;
+// do{
+//     console.log("i = " + i);
+//     i++;
+// }while(i <= 5);
+   
+
+// for-of loop
+// let str = "NavdeepKaur";
+
+// for(let val of str){
+//     console.log(val);
+// }
+
+//for-in loop
+// let student = {
+//     name : "navdeep",
+//     Age : 25,
+//     cgpa: 8.52,
+//     isPass : true,
+// };
+
+// for(let i in student){
+//    console.log(i)
+// }
